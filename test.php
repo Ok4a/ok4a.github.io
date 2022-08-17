1b5d811814bbbe41de9d4fa57024052d62269345
Ok4a <owkaa@outlook.dk>
2022-08-18 01:04:42 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,8 @@
 <!DOCTYPE html>
 <html>
 <body>
-  <?php
-$i=1;
-while ($i < 10) {
-  echo "Whether you're up in the clouds or going way underground, it's easy to take your music with you whereever you go. With Spotify Premium, you can save your favorite songs to your phone and listen offline. That means you can play anywhere, anytime without using any data. And right now, you can try Premium free for 30 days. Ready to make the move? Tap the banner to learn more.<br><br>";
-  $i++;
-}
- ?>
+  <h1>TEST</h1>
+  <p>I'm hosted with GitHub Pages.</p>
+  <br><a href="google.com">test</a> <br>
 </body>
 </html>
