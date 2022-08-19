e11df399574b6038927add5e7e3d5d2a44c983d4
Ok4a <owkaa@outlook.dk>
2022-08-19 19:03:39 +0200

    d

--- switch.py
+++ switch.py
@@ -6,9 +6,9 @@ with open('Switch.csv') as csv_file:
         csv_reader = sorted(csv_reader, key=operator.itemgetter(0))
         tablewidth = 6
         count = 0
+        sid ='id="switch_td"'
 
-
-        txt.write('<!DOCTYPE html> \n<html>\n<link rel="stylesheet" href="style.css">\n<style>td{text-align: top;}</style\n<title>Boardgames</title>\n<body>')
+        txt.write('<!DOCTYPE html> \n<html>\n<link rel="stylesheet" href="style.css">\n<title>Boardgames</title>\n<body>')
         txt.write(f'<table id="maintable">\n \t <tr> <th colspan="{tablewidth}">Nintendo Switch Games</th> </tr> \n \t <tr> \n')
 
         for row in csv_reader:
@@ -16,7 +16,7 @@ with open('Switch.csv') as csv_file:
                  txt.write(f'\t </tr> \n \t <tr>\n')
                  count = 0
 
-            txt.write(f'\t\t <td> \n \t\t\t  <table  id="innertable"><tr> <td><img src="{row[2]}"> </td></tr> <tr> <td>{row[0]}</td></tr></table></td> \n')
+            txt.write(f'\t\t <td {sid}> \n \t\t\t  <table  id="innertable"><tr> <td {sid}><img src="{row[2]}"> </td></tr> <tr> <td {sid}>{row[0]}</td {sid}></tr></table></td> \n')
             count += 1
 
         txt.write('</tr>\n</table>')
