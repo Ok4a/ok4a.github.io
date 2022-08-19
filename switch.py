b2dd27f5b5ea6d9a1519321f08315c69ae612a5f
Ok4a <owkaa@outlook.dk>
2022-08-19 17:48:04 +0200

    Update switch.py

--- switch.py
+++ switch.py
@@ -10,13 +10,13 @@ with open('Switch.csv') as csv_file:
         
         txt.write(f'<table id="maintable">\n \t <tr colspan="{tablewidth}"> <th>Bog liste</th> </tr> \n \t <tr> \n')
         for row in csv_reader:
-            if row[0] == "book":
-                if  count == tablewidth:
-                    txt.write(f'\t </tr> \n \t <tr>\n')
-                    count = 0
-                else:
-                    txt.write(f'\t\t <td> \n \t\t\t  <table><tr> <td><img src="{row[7]}"> </td></tr> <tr> <td>{row[2]} <br> {row[5]} </td></tr></table></td> \n')
-                    count += 1
+
+            if  count == tablewidth:
+                txt.write(f'\t </tr> \n \t <tr>\n')
+                count = 0
+
+            txt.write(f'\t\t <td> \n \t\t\t  <table><tr> <td><img src="{row[7]}"> </td></tr> <tr> <td>{row[2]} <br> {row[5]} </td></tr></table></td> \n')
+            count += 1
 
         txt.write('</tr>\n</table>')
 
