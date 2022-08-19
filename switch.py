2c978d6576f2e2561e0ea083a84793f07d308158
Ok4a <owkaa@outlook.dk>
2022-08-19 18:52:49 +0200

    switch

--- switch.py
+++ switch.py
@@ -1,23 +1,22 @@
 import csv , operator
 
 with open('Switch.csv') as csv_file:
-    with open('switch.txt', 'w') as txt:
+    with open('switch.html', 'w') as txt:
         csv_reader = csv.reader(csv_file, delimiter=';')
-        csv_reader = sorted(csv_reader, key=operator.itemgetter(3))
-        csv_reader = sorted(csv_reader, key=operator.itemgetter(5))
-
-        line_count = -1
+        csv_reader = sorted(csv_reader, key=operator.itemgetter(0))
         tablewidth = 6
         count = 0
-        
-        txt.write(f'<table id="maintable">\n \t <tr colspan="{tablewidth}"> <th>Bog liste</th> </tr> \n \t <tr> \n')
+
+
+        txt.write(f'<!DOCTYPE html> \n<html>\n<link rel="stylesheet" href="style.css">\n<title>Boardgames</title>\n<body>')
+        txt.write(f'<table id="maintable">\n \t <tr colspan="{tablewidth}"> <th>Brætpils liste</th> </tr> \n \t <tr> \n')
+
         for row in csv_reader:
-
             if  count == tablewidth:
-                txt.write(f'\t </tr> \n \t <tr>\n')
-                count = 0
+                 txt.write(f'\t </tr> \n \t <tr>\n')
+                 count = 0
 
-            txt.write(f'\t\t <td> \n \t\t\t  <table><tr> <td><img src="{row[7]}"> </td></tr> <tr> <td>{row[2]} <br> {row[5]} </td></tr></table></td> \n')
+            txt.write(f'\t\t <td> \n \t\t\t  <table  id="innertable"><tr> <td><img src="{row[2]}"> </td></tr> <tr> <td>{row[0]}</td></tr></table></td> \n')
             count += 1
 
         txt.write('</tr>\n</table>')
