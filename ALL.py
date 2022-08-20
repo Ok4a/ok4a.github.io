de601d1090c5c6216591f34f0ed4d0627a930bd3
Ok4a <owkaa@outlook.dk>
2022-08-20 15:59:28 +0200

    k

--- ALL.py
+++ ALL.py
@@ -5,7 +5,7 @@ tablewidth = 6
 sid ='id="switch_td"'
 cid ='id=cont_td'
 
-start_string = '<!DOCTYPE html> \n<html  lang="en" dir="ltr">\n<link rel="stylesheet" href="style.css"> <head> <meta charset="utf-8"> </head>\n<title>Boardgames</title>\n<body>'
+start_string = '<!DOCTYPE html> \n<html lang="en" dir="ltr">\n<link rel="stylesheet" href="style.css"> <head> <meta charset="utf-8"> </head>\n'
 topbar_string = '<table id="topbar"> <tr> <th colspan="3">Mine lister over brætspil, bøger og spil til Nintendo Switch</th> </tr> <tr><td><a href="boardgame.html">Boardgames</a></td><td ><a href="books.html">Books</a></td><td><a href="switch.html">Nindtendo Switch</a></td></tr> </table>'
 
 #Boardgames
@@ -16,7 +16,7 @@ with open('Boardgame.csv') as boardgame_csv:
 
         count = 0
 
-        boardgame_html.write(start_string)
+        boardgame_html.write(f'{start_string} <title>Boardgames</title>\n<body>')
         boardgame_html.write(topbar_string)
         boardgame_html.write(f'<table id="maintable">\n \t <tr> <th colspan="{tablewidth}">Boardgames</th> </tr> \n \t <tr> \n')
 
@@ -45,7 +45,7 @@ with open('Books.csv') as book_csv:
 
         count = 0
 
-        book_html.write(start_string)
+        book_html.write(f'{start_string} <title>Bøger</title>\n<body>')
         book_html.write(topbar_string)
         book_html.write(f'<table id="maintable">\n \t <tr> <th colspan="{tablewidth}">Books</th> </tr> \n \t <tr> \n')
         
@@ -74,7 +74,7 @@ with open('Switch.csv') as switch_csv:
 
 
 
-        switch_html.write(start_string)
+        switch_html.write(f'{start_string} <title>Switch Games</title>\n<body>')
         switch_html.write(topbar_string)
         switch_html.write(f'<table id="maintable">\n \t <tr> <th colspan="{tablewidth}">Nintendo Switch Games</th> </tr> \n \t <tr> \n')
 
@@ -85,6 +85,9 @@ with open('Switch.csv') as switch_csv:
 
             switch_html.write(f'\t\t <td {sid}> \n \t\t\t  <table  id="innertable"><tr> <td {cid}><img src="{row[2]}"> </td></tr> <tr> <td {cid}>{row[0]}</td {sid}></tr></table></td> \n')
             count += 1
+            if row[0] == 'Pokémon Shield':
+                with open('test.txt','w') as text:
+                    text.write(row[0])
 
         switch_html.write('</tr>\n</table>')
 
