74b2223daa20c934d8efb4eaea6f3407cb08829e
Ok4a <owkaa@outlook.dk>
2024-03-16 10:52:42 +0100

    dw

--- sidebar.js
+++ sidebar.js
@@ -24,6 +24,7 @@ document.write('\
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     // true for mobile device
     document.write("mobile device");
+    window.location.href = "http://www.w3schools.com";
   }else{
     // false for not mobile device
     document.write("not mobile device");
