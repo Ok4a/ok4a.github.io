aad5cf412d191170a78ed8354cce1530f34a2352
Okaa <owkaa@outlook.dk>
2026-01-05 09:06:27 +0100

    New entry added

--- ALL.py
+++ ALL.py
@@ -7,12 +7,14 @@ Ser, Ent = loadData(csv_name, createFun = createLegoData)
 writeHTML(Ent, csv_name, csv_name)
 
 for series in Ser:
+    print(series.name)
     writeHTML(series.entries, series.name, f'{series.name}_{csv_name}')
 
 csv_name = 'switch'
 Ser, Ent = loadData(csv_name, createFun = createSwitchData)
 writeHTML(Ent, csv_name, csv_name)
 for series in Ser:
+    print(series.name)
     writeHTML(series.entries, series.name, f'{series.name}_{csv_name}')
 
 
@@ -21,6 +23,7 @@ csv_name = 'boardgame'
 Ser, Ent = loadData(csv_name, createFun = createBoardgameData)
 writeHTML(Ent, 'Brætspil', csv_name, compress_series_entries = True, start_compressed = True)
 for series in Ser:
+    print(series.name)
     writeHTML(series.entries, series.name, f'{series.name}_{csv_name}', compress_series_entries = True)
 
 
@@ -28,4 +31,5 @@ csv_name = 'books'
 Ser, Ent = loadData(csv_name, createFun = createBookData)
 writeHTML(Ent, 'Bøger', csv_name, compress_series_entries = True, start_compressed = True)
 for series in Ser:
+    print(series.name)
     writeHTML(series.entries, series.name, f'{series.name}_{csv_name}', compress_series_entries = True)
